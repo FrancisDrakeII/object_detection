b2ab7c35b9174f4ff61c8c84bca3498a5dcdca79
FrancisDrakeII <55567729+FrancisDrakeII@users.noreply.github.com>
2022-02-01 23:43:04 -0500

    update

--- models/research/object_detection/Object_detection_webcam.py
+++ models/research/object_detection/Object_detection_webcam.py
@@ -33,10 +33,6 @@ PATH_TO_LABELS = os.path.join(CWD_PATH,'training','labelmap.pbtxt')
 NUM_CLASSES = 6
 
 ## Load the label map.
-# Label maps map indices to category names, so that when our convolution
-# network predicts `5`, we know that this corresponds to `king`.
-# Here we use internal utility functions, but anything that returns a
-# dictionary mapping integers to appropriate string labels would be fine
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
